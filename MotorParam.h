c0499655948bb7ba8d1b8c2ea254eeb6de7fdf9a
Wang Wenda <wwd7086@gmail.com>
2015-04-11 23:22:37 -0400

    new unloader param

--- MotorParam.h
+++ MotorParam.h
@@ -22,7 +22,7 @@ const int yAccel = 500;
 const int yDeAccel = 500;
 
 //range of linear actuator
-const long rangeLMin = 20;
+const long rangeLMin = 75;
 const long rangeLMax = 865; //for cdr
 
 #endif
